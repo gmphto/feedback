5b4b2409e6068661028637219996b2d346eb8152
Gift Mphato <g.mphato92@gmail.com>
2026-09-05 21:53:18 +0100

    clean up reducer

--- client/src/projects/state/reducer.ts
+++ client/src/projects/state/reducer.ts
@@ -1,15 +1,8 @@
-import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
-import { emptyDraft } from '../fields';
-import type { EditorState } from '../editor/state/state';
+import { type PayloadAction } from '@reduxjs/toolkit';
 import type { ProjectState } from './state';
 import type { Project } from '../types/project';
-import type { GuidString } from '../../shared/util/types';
-// import { PayloadA}
-
 
 interface StartEditPayload {
-
-    // projectKey: GuidString
 
     /** The project to open in the editor */
     projectToOpen: Project
@@ -58,26 +51,3 @@ export const projectReducer = {
         handleOpenEditor(state, { ...projectToOpen, isNew: true, isReadOnly: readOnlySession });
     },
 }
-
-// const projectsSlice = createSlice({
-//   name: 'projects',
-//   initialState,
-//   reducers: {
-//     startCreate(state) {
-//       state.view = 'editor';
-//       state.editor = { activeKey: undefined, original: undefined, draft: emptyDraft() };
-//     },
-//     startEdit(state, { payload }: PayloadAction<NonNullable<EditorState['original']>>) {
-//       const { id, version: _version, ...draft } = payload;
-//       state.view = 'editor';
-//       state.editor = { activeKey: id, original: { ...payload }, draft };
-//     },
-//     stopEdit(state) {
-//       state.view = 'dashboard';
-//       state.editor = { activeKey: undefined, original: undefined, draft: undefined };
-//     },
-//   },
-// });
-
-export const { startCreate, startEdit, stopEdit } = projectsSlice.actions;
-export const projectsReducer = projectsSlice.reducer;
